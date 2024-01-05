7a482134017a91f2794e1f2bfa74788ff23dd002
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-01-05 23:47:38 +0300

    -mpackage making

--- website/__init__.py
+++ website/__init__.py
@@ -1,1 +1,14 @@
-from flask import flask
+from flask import Flask
+from .views import views
+from .auth import auth
+
+
+def create_app():  # application factory - func creates a Flask app instance
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'djncdoncen'
+
+    # registering blueprints
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    return app
