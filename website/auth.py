fff97817e55c86a4daf3981e88754d152a5d1815
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-01-08 10:00:35 +0300

    -mmore templates

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
-def login():
-    return "<h1>auth..</h1>"
-
-
 @auth.route('/logout')
 def logout():
-    return "<h1>Logging out</h1>"
+    return render_template("logout.html")
+
+
+@auth.route('/log-in')
+def login():
+    return render_template("log_in.html")
 
 
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template("sign_up.html")
