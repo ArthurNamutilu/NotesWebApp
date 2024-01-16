145dc6ffb613598c2e414d530338b96d054dfd88
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-01-16 23:48:29 +0300

    -mconfiguration

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,32 @@
 from flask import Flask
+from os import path
 from .views import views
 from .auth import auth
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():  # application factory - func creates a Flask app instance
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'djncdoncen'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     # registering blueprints
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+    create_database(app)
+
     return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+        print('Okay Database created')
