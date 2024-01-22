9e43e14c0b68e2bb3eaf3fa2eb775e44fd6da593
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-01-22 13:46:05 +0300

    -mcreated the sign-up function

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from .db import db
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 auth = Blueprint('auth', __name__)
 
@@ -19,19 +23,28 @@ def login():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstname = request.form.get('email')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
-            flash('Email must be greater than 3 chracters', category='error')
-        elif len(firstname) < 4:
-            flash('First name must be greater than 3 characters', category='error')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
-            flash('Your password do not match', category='error')
+            flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
-            flash('Password must be atleast 7 characters', category='error')
+            flash('Password must be at least 7 characters.', category='error')
         else:
-            flash('Account succesfully created', category='success')
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+                password1, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            # login_user(new_user, remember=True)
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
 
     return render_template("sign_up.html")
